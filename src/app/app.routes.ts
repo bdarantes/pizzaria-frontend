744c80769dd9dc46ba7cbd6b6522b7b160f263fd
BrazD <brazdanarantes@gmail.com>
2026-04-06 08:04:04 -0300

    feat: create login form interface with data binding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { HomeCliente } from './pages/home-cliente/home-cliente';
+import { PainelAdmin } from './pages/painel-admin/painel-admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+    { path: 'login', component: Login },
+    { path: 'cliente', component: HomeCliente },
+    { path: 'admin', component: PainelAdmin }
+];
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class Login {
+  email: string = '';
+  senha: string = '';
+
+  fazerLogin() {
+    console.log('Tentativa de login iniciada!');
+    console.log('E-mail capturado', this.email);
+    console.log('Senha capturada', this.senha);
+  }
+}
